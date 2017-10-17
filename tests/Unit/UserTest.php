09e07e84ff4a17b2c530381c622dcaebe5ab18e9
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-17 14:30:48 -0600

    New unit test: UserTest::User_can_get_its_most_recently_loaded_list()

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -7,10 +7,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use \App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use \App\Models\TaskList;
+use Carbon\Carbon;
 
 class UserTest extends TestCase
 {
-    public function testThatUserCanLogin()
+    /** @test */
+    public function User_can_login()
     {
         $user = User::create([
             'name' => 'testuser',
@@ -27,7 +29,8 @@ class UserTest extends TestCase
         $user->delete();
     }
 
-    public function testThatUserCanGetItsLists()
+    /** @test */
+    public function User_can_get_its_lists()
     {
         $user = User::create([
             'name' => 'testuser',
@@ -49,6 +52,43 @@ class UserTest extends TestCase
 
         $this->assertCount(2, $lists);
         $this->assertEquals($lists, TaskList::where('user_id', Auth::id())->get());
+
+        Auth::logout();
+        $list1->delete();
+        $list2->delete();
+        $user->delete();
+    }
+
+    /** @test */
+    public function User_can_get_its_most_recently_loaded_list()
+    {
+        $user = User::create([
+            'name' => 'testuser',
+            'email' => 'testuser@example.com',
+            'password' => bcrypt('password')
+        ]);
+        Auth::login($user);
+
+        $list1 = TaskList::create([
+            'user_id' => Auth::id(),
+            'name' => 'list1'
+        ]);
+        $list2 = TaskList::create([
+            'user_id' => Auth::id(),
+            'name' => 'list2'
+        ]);
+
+        $list1->last_time_loaded = (Carbon::today())->toDateTimeString();
+        // $list1->last_time_loaded = (new DateTime('today'))->format('Y-m-d H:i:s');
+        $list1->save();
+
+        $list2->last_time_loaded = (Carbon::yesterday())->toDateTimeString();
+        // $list2->last_time_loaded = (new DateTime('yesterday'))->format('Y-m-d H:i:s');
+        $list2->save();
+
+        $currentList = $user->getCurrentList();
+
+        $this->assertEquals($currentList, $list1);
 
         Auth::logout();
         $list1->delete();
