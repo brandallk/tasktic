86027f492da47bd8e95af5266de032f56437a841
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-18 13:20:15 -0600

    New unit test: UserTest::User_can_register()

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -29,6 +29,28 @@ class UserTest extends TestCase
     }
 
     /** @test */
+    public function User_can_register()
+    {
+        $userDetails = [
+            'name' => 'testuser',
+            'email' => 'testuser@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ];
+        
+        $response = $this->post('/register', $userDetails);
+
+        $user = User::where('name', 'testuser')->first();
+
+        $this->assertTrue(Auth::check());
+        $this->assertEquals(Auth::id(), $user->id);
+        $this->assertEquals(Auth::user()->name, 'testuser');
+
+        Auth::logout();
+        $user->delete();
+    }
+
+    /** @test */
     public function User_can_get_its_lists()
     {
         $user = User::create([
