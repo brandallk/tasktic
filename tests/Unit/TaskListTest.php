a03abd2dc91d6ca4edcf8335eac90a7f74fbf771
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-19 22:05:02 -0600

    New unit test: TaskListTest::a_TaskList_has_a_name()

--- tests/Unit/TaskListTest.php
+++ tests/Unit/TaskListTest.php
@@ -20,6 +20,74 @@ class TaskListTest extends TestCase
         $list->user()->associate($user);
         $list->save();
 
-        $this->assertEquals($list->user->name, 'testuser');
+        $this->assertEquals('testuser', $list->user->name);
     }
+
+    /** @test */
+    public function a_TaskList_has_a_name()
+    {
+        $list = factory(TaskList::class)->create(['name' => 'New List']);
+
+        $this->assertEquals('New List', $list->name);
+    }
+
+    // /** @test */
+    // public function a_TaskList_can_add_and_access_list_Categories()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_new_TaskList_gets_true_autodelete_status_by_default()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_default_TaskList_can_be_created()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_new_default_TaskList_is_named_after_the_current_date()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_new_default_TaskList_gets_false_saved_status()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_new_default_TaskList_has_a_Category_named_none()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_custom_TaskList_can_be_created()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_new_custom_TaskList_gets_true_saved_status()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_TaskList_can_add_and_access_list_elements()
+    // {
+        
+    // }
+
+    // /** @test */
+    // public function a_list_element_can_be_deleted()
+    // {
+        
+    // }
 }
