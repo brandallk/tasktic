a7e013bcabca279ba49f07eb75d5ab3f87b69d0a
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-21 22:15:23 -0600

    Create model manager class App\Models\Managers\ItemManager

---
+++ app/Models/Managers/ItemManager.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models\Managers;
+
+use App\Models\Task;
+use App\Models\DeadlineItem;
+use App\Models\DetailItem;
+use App\Models\LinkItem;
+
+class ItemManager
+{
+    protected static function item(string $type)
+    {
+        $itemType = null;
+
+        switch ($type) {
+            case 'deadline':
+                $itemType = 'DeadlineItem';
+                break;
+            case 'detail':
+                $itemType = 'DetailItem';
+                break;
+            case 'link':
+                $itemType = 'LinkItem';
+                break;
+        }
+
+        return $itemType;
+    }
+
+    public static function newItem(string $type, string $content, Task $task)
+    {
+        $itemType = self::item($type);
+
+        return $itemType::newItem($task, $content);
+    }
+
+    public static function deleteItem(string $type, int $uniqueID)
+    {
+        $itemType = self::item($type);
+
+        $item = $itemType::where('list_element_id', $uniqueID)->first();
+        $itemType::deleteItem($item);
+    }
+}
