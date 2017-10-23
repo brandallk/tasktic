300fb63d46d5732ce6054be3cf0951145182031e
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-23 11:46:48 -0600

    New unit test: ListElementTest::a_List_Element_has_a_type_a_name_and_a_unique_id()

--- tests/Unit/ListElementTest.php
+++ tests/Unit/ListElementTest.php
@@ -22,13 +22,17 @@ class ListElementTest extends TestCase
         $this->assertDatabaseHas('list_elements', ['task_list_id' => $list->id]);
     }
 
-    // /** @test */
-    // public function a_List_Element_has_a_type_a_name_and_a_unique_id()
-    // {
-    //     // given 
-    //     // when 
-    //     // then 
-    // }
+    /** @test */
+    public function a_List_Element_has_a_type_a_name_and_a_unique_id()
+    {
+        $user = factory(User::class)->create();
+        $list = TaskList::newTaskList($user, 'New List');
+        $listElement = ListElement::addListElement($list, 'task', 'Task Name', 'dnor794#dnu8v');
+        
+        $this->assertEquals('task', $listElement->type);
+        $this->assertEquals('Task Name', $listElement->name);
+        $this->assertEquals('dnor794#dnu8v', $listElement->unique_id);
+    }
 
     // /** @test */
     // public function a_List_Element_belongs_to_a_TaskList()
