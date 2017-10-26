798d00419677f7550536f378b71a1defe8961531
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-26 09:00:54 -0600

    Add database transactions to App\Models\TaskItem

--- app/Models/TaskItem.php
+++ app/Models/TaskItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Task;
 use App\Models\Interfaces\iItem;
@@ -20,16 +21,18 @@ class TaskItem extends Model
 
     public static function addItem(iItem $item, Task $task)
     {
-        $taskItem = self::create([
-            'task_id' => $task->id,
-            'type' => $item->type,
-            'unique_id' => $item->list_element_id
-        ]);
+        return DB::transaction(function () use ($item, $task) {
+            $taskItem = self::create([
+                'task_id' => $task->id,
+                'type' => $item->type,
+                'unique_id' => $item->list_element_id
+            ]);
 
-        $taskItem->task()->associate($task);
-        $taskItem->save();
+            $taskItem->task()->associate($task);
+            $taskItem->save();
 
-        return $taskItem;
+            return $taskItem;
+        });
     }
 
     public static function removeItem(iItem $item, Task $task)
