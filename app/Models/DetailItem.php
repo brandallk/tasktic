54515917ce30dad1ef6df8c4fbd7cb57df04f04e
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-26 09:07:46 -0600

    Add database transactions to App\Models\DetailItem

--- app/Models/DetailItem.php
+++ app/Models/DetailItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use DB;
 use App\Models\Item;
 use App\Models\ListElement;
 use App\Models\Interfaces\iItem;
@@ -14,12 +15,14 @@ class DetailItem extends Item
 
     public function updateItem(iItem $item, Task $task, $content)
     {
-        $item->detail = $content;
-        $item->save();
+        return DB::transaction(function () use ($item, $task, $content) {
+            $item->detail = $content;
+            $item->save();
 
-        $list = $task->subcategory->category->taskList;
-        ListElement::updateListElement($list, $content, $item->list_element_id);
+            $list = $task->subcategory->category->taskList;
+            ListElement::updateListElement($list, $content, $item->list_element_id);
 
-        return $item;
+            return $item;
+        });
     }
 }
