2c549ab686c1b0b22642dceaaecd140dee3937c1
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-27 12:42:01 -0600

    Add doc-block comments to App\Models\TaskList

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -11,34 +11,72 @@ use App\Models\Category;
 
 class TaskList extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'user_id', 'name', 'saved', 'autodelete'
     ];
 
+    /**
+     * Define an Eloquent ORM one-to-many reverse relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo  TaskList belongs-to User.
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    public function categories()
-    {
-        return $this->hasMany(Category::class);
-    }
-
+    /**
+     * Define an Eloquent ORM one-to-many relationship. ListElements include Categories, Subcategories,
+     * Tasks, DeadlineItems, DetailItems, and LinkItems. The ListElements collection provides a single
+     * list of objects that belong to the TaskList, including their names and uniqueIDs.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany  TaskList has-many ListElements.
+     */
     public function listElements()
     {
         return $this->hasMany(ListElement::class);
     }
 
+    /**
+     * Define an Eloquent ORM one-to-many relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany  TaskList has-many Categories.
+     */
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    /**
+     * Create a new unsaved TaskList that takes the current date as name.
+     *
+     * @param App\Models\User $user
+     *
+     * @return App\Models\TaskList
+     */
     public static function newDefaultTaskList(User $user)
     {
         // Get a string for the current day's date formatted like "Wednesday, October 18th"
         $today = (\Carbon\Carbon::now())->format('l\, F jS');
 
-        // Create a new unsaved TaskList using today's date as 'name'
         return self::newTaskList($user, $today, false);
     }
 
+    /**
+     * Create a new TaskList with the given name, assigned to the given User. Use a database
+     * transaction so operations will automatically rollback if a failure occurs.
+     *
+     * @param App\Models\User $user  The User to which the TaskList belongs.
+     * @param string $name  The name assigned to the TaskList.
+     * @param bool $saved  Indicates whether the TaskList is 'saved' or not. Equals TRUE by default.
+     *
+     * @return App\Models\TaskList
+     */
     public static function newTaskList(User $user, string $name, bool $saved = true)
     {
         return DB::transaction(function () use ($user, $name, $saved) {
@@ -58,6 +96,14 @@ class TaskList extends Model
         });
     }
 
+    /**
+     * Update a TaskList's name.
+     *
+     * @param App\Models\TaskList $list  The TaskList to be updated.
+     * @param string $name  The new name assigned to the TaskList.
+     *
+     * @return App\Models\TaskList
+     */
     public function updateTaskList(TaskList $list, string $name)
     {
         $list->name = $name;
@@ -66,6 +112,14 @@ class TaskList extends Model
         return $list;
     }
 
+    /**
+     * Update an unsaved TaskList's name to the current date and return TRUE. If the TaskList
+     * is already 'saved', do nothing and return FALSE.
+     *
+     * @param App\Models\TaskList $list  The TaskList to be updated.
+     *
+     * @return bool
+     */
     public function setDefaultName(TaskList $list)
     {
         if (!$list->saved) {
@@ -78,6 +132,31 @@ class TaskList extends Model
         return false;
     }
 
+    /**
+     * Update the given TaskList's 'last_time_loaded' property to the current DateTime.
+     *
+     * @param App\Models\TaskList $list  The TaskList to be updated.
+     *
+     * @return void
+     */
+    public function updateLastTimeLoaded(TaskList $list)
+    {
+        $now = (\Carbon\Carbon::now())->toDateTimeString();
+
+        $list->last_time_loaded = $now;
+        $list->save();
+
+        return $list;
+    }
+
+    /**
+     * Delete the given TaskList and all Categories belonging to it. Use a database transaction
+     * so operations will automatically rollback if a failure occurs.
+     *
+     * @param App\Models\TaskList $list  The TaskList to be deleted.
+     *
+     * @return bool  TRUE
+     */
     public static function deleteTaskList(TaskList $list)
     {
         return DB::transaction(function () use ($list) {
@@ -90,15 +169,5 @@ class TaskList extends Model
 
             return true;
         });
-    }
-
-    public function updateLastTimeLoaded(TaskList $list)
-    {
-        $now = (\Carbon\Carbon::now())->toDateTimeString();
-
-        $list->last_time_loaded = $now;
-        $list->save();
-
-        return $list;
     }
 }
