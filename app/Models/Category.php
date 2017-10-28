1285744e24d4f6958ad2ddf2d0903a04b240a9ab
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-28 09:52:11 -0600

    Add doc-block comments to App\Models\Category and App\Models\Subcategory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,24 +11,48 @@ use App\Models\Subcategory;
 
 class Category extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'task_list_id', 'name', 'list_element_id'
     ];
 
+    /**
+     * Define an Eloquent ORM one-to-many reverse relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo  Category belongs-to TaskList.
+     */
     public function taskList()
     {
         return $this->belongsTo(TaskList::class);
     }
 
+    /**
+     * Define an Eloquent ORM one-to-many relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany  Category has-many Subcategories.
+     */
     public function subcategories()
     {
         return $this->hasMany(Subcategory::class);
     }
 
+    /**
+     * Create a new Category with name === NULL, with a Subcategory also named NULL, assigned to
+     * the given TaskList. Use a database transaction so operations will automatically rollback if
+     * a failure occurs.
+     *
+     * @param App\Models\TaskList $list  The TaskList to which the Category belongs.
+     *
+     * @return App\Models\Category
+     */
     public static function newDefaultCategory(TaskList $list)
     {
         return DB::transaction(function () use ($list) {
-            // Create a new Category with 'name' == null
+            
             $defaultCategory = self::newCategory($list, null);
 
             Subcategory::newDefaultSubcategory($defaultCategory);
@@ -37,10 +61,19 @@ class Category extends Model
         });
     }
 
-    /** @param $name  is either a string or null */
+    /**
+     * Create a new Category with the given name, assigned to the given TaskList. Use a database
+     * transaction so operations will automatically rollback if a failure occurs.
+     *
+     * @param App\Models\TaskList $list  The TaskList to which the Category belongs.
+     * @param mixed $name  The name assigned to the Category: either a string or NULL.
+     *
+     * @return App\Models\Category
+     */
     public static function newCategory(TaskList $list, $name)
     {
         return DB::transaction(function () use ($list, $name) {
+            // A unique 13-character string to distinguish the Category from other ListElements
             $uniqueID = uniqid();
 
             $category = self::create([
@@ -52,18 +85,29 @@ class Category extends Model
             $category->taskList()->associate($list);
             $category->save();
 
+            // Also create a ListElement on the TaskList corresponding to the new Category.
             ListElement::addListElement($list, 'category', $name, $uniqueID);
 
             return $category;
         });
     }
 
-    public function updateCategory(Category $category, string $name)
+    /**
+     * Update the given Category's name. Use a database transaction so operations will
+     * automatically rollback if a failure occurs.
+     *
+     * @param App\Models\Category $category  The Category to be updated.
+     * @param mixed $name  The Category's new name.
+     *
+     * @return App\Models\Category
+     */
+    public function updateCategory(Category $category, $name)
     {
         return DB::transaction(function () use ($category, $name) {
             $category->name = $name;
             $category->save();
 
+            // Also update the corresponding ListElement name.
             $list = $category->taskList;
             ListElement::updateListElement($list, $name, $category->list_element_id);
 
@@ -71,6 +115,14 @@ class Category extends Model
         });
     }
 
+    /**
+     * Delete the given Category and all Subcategories belonging to it. Use a database transaction
+     * so operations will automatically rollback if a failure occurs.
+     *
+     * @param App\Models\Category $category  The Category to be deleted.
+     *
+     * @return bool
+     */
     public static function deleteCategory(Category $category)
     {
         return DB::transaction(function () use ($category) {
@@ -84,6 +136,7 @@ class Category extends Model
             // Note: important that the Category is deleted AFTER its child Subcategories are deleted
             $category->delete();
 
+            // Also delete the corresponding ListELement.
             ListElement::deleteListElement($list, $uniqueID);
 
             return true;
--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -11,34 +11,66 @@ use App\Models\Task;
 
 class Subcategory extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'category_id', 'name', 'list_element_id'
     ];
 
+    /**
+     * Define an Eloquent ORM one-to-many reverse relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo  Subcategory belongs-to Category.
+     */
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
+    /**
+     * Define an Eloquent ORM one-to-many relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany  Subcategory has-many Tasks.
+     */
     public function tasks()
     {
         return $this->hasMany(Task::class);
     }
 
+    /**
+     * Create a new Subcategory with name === NULL, assigned to the given Category. Use a database
+     * transaction so operations will automatically rollback if a failure occurs.
+     *
+     * @param App\Models\Category $category  The Category to which the Subcategory belongs.
+     *
+     * @return App\Models\Subcategory
+     */
     public static function newDefaultSubcategory(Category $category)
     {
         return DB::transaction(function () use ($category) {
-            // Create a new Subcategory with 'name' == null
+            
             $defaultSubcategory = self::newSubcategory($category, null);
 
             return $defaultSubcategory;
         });
     }
 
-    /** @param $name  is either a string or null */
+    /**
+     * Create a new Subcategory with the given name, assigned to the given Category. Use a database
+     * transaction so operations will automatically rollback if a failure occurs.
+     *
+     * @param App\Models\Category $category  The Category to which the Subcategory belongs.
+     * @param mixed $name  The name assigned to the Subcategory: either a string or NULL.
+     *
+     * @return App\Models\Subcategory
+     */
     public static function newSubcategory(Category $category, $name)
     {
         return DB::transaction(function () use ($category, $name) {
+            // A unique 13-character string to distinguish the Subcategory from other ListElements
             $uniqueID = uniqid();
 
             $subcategory = self::create([
@@ -50,6 +82,7 @@ class Subcategory extends Model
             $subcategory->category()->associate($category);
             $subcategory->save();
 
+            // Also create a ListElement on the TaskList corresponding to the new Subcategory.
             $list = $category->taskList;
             ListElement::addListElement($list, 'subcategory', $name, $uniqueID);
 
@@ -57,12 +90,22 @@ class Subcategory extends Model
         });
     }
 
+    /**
+     * Update the given Subcategory's name. Use a database transaction so operations will
+     * automatically rollback if a failure occurs.
+     *
+     * @param App\Models\Subcategory $subcategory  The Subcategory to be updated.
+     * @param mixed $name  The Subcategory's new name.
+     *
+     * @return App\Models\Subcategory
+     */
     public function updateSubcategory(Subcategory $subcategory, string $name)
     {
         return DB::transaction(function () use ($subcategory, $name) {
             $subcategory->name = $name;
             $subcategory->save();
 
+            // Also update the corresponding ListElement name.
             $list = $subcategory->category->taskList;
             ListElement::updateListElement($list, $name, $subcategory->list_element_id);
 
@@ -70,6 +113,14 @@ class Subcategory extends Model
         });
     }
 
+    /**
+     * Delete the given Subcategory and all Tasks belonging to it. Use a database transaction
+     * so operations will automatically rollback if a failure occurs.
+     *
+     * @param App\Models\Subcategory $subcategory  The Subcategory to be deleted.
+     *
+     * @return bool
+     */
     public static function deleteSubcategory(Subcategory $subcategory)
     {
         return DB::transaction(function () use ($subcategory) {
@@ -83,6 +134,7 @@ class Subcategory extends Model
             // Note: important that the Subcategory is deleted AFTER its child Tasks are deleted
             $subcategory->delete();
 
+            // Also delete the corresponding ListELement.
             ListElement::deleteListElement($list, $uniqueID);
 
             return true;
