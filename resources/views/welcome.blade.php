06e1af75027c9ece546ea9a092c986a99af57919
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-29 11:03:30 -0600

    Update the 'welcome' view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,23 +5,38 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Welcome</title>
+        <title>Welcome to Tasktic!</title>
         
     </head>
     <body>
-            @if (Route::has('login'))
-                <div>
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+            <nav>
+                <ul>
+                    <li><a href="{{ route('about') }}">About Tasktic</a></li>
+                </ul>
+            </nav>
 
-            <div>
-                Welcome Page
+            <hr>
+
+            <div class="welcome">
+                <h1>Welcome to Tasktic!</h1>
+                <span>Get Organized. Create your daily todo-list online!</span>
+
+                @if (Route::has('login'))
+                    <div class="authLinks">
+                        @auth
+                            <button type="button" name="home">
+                                <a href="{{ url('/home') }}">Home</a>
+                            </button>
+                        @else
+                            <button type="button" name="login">
+                                <a href="{{ route('login') }}">Login</a>
+                            </button>
+                            <button type="button" name="register">
+                                <a href="{{ route('register') }}">Register</a>
+                            </button>
+                        @endauth
+                    </div>
+                @endif
             </div>
     </body>
 </html>
