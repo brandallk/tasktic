0478395dcc2cd70735044cdced0503a7ace6bd16
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-29 21:47:13 -0600

    Add 'store' method to ListController

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\TaskList;
 
 class ListController extends Controller
 {
     /**
-     * Show the lists index page.
+     * Show an index of the all the user's (default and) saved TaskLists.
      *
      * @return \Illuminate\Http\Response
      */
@@ -20,6 +21,32 @@ class ListController extends Controller
             ];
 
             return view('list.index', $data);
+
+        } catch (\Throwable $e) {
+            return redirect()->back();
+        } catch (\Exception $e) {
+            return redirect()->back();            
+        }
+    }
+
+    /**
+     * Create a new TaskList instance and show it.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        try {
+            $user = Auth::user();
+            $name = $request->name;
+
+            $newList = TaskList::newTaskList($user, $name);
+
+            return $this->show($newList);
 
         } catch (\Throwable $e) {
             return redirect()->back();
