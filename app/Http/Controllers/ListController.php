5f66f39525ab9393c37aca1f16cb8fbeb6aa616c
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-30 18:06:29 -0600

    Add 'update' method to ListController

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -32,6 +32,8 @@ class ListController extends Controller
     /**
      * Create a new TaskList instance and show it.
      *
+     * @param Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -47,6 +49,34 @@ class ListController extends Controller
             $newList = TaskList::newTaskList($user, $name);
 
             return $this->show($newList); // test that response is the list.show view (after show method is added)
+
+        } catch (\Throwable $e) {
+            return redirect()->back();
+        } catch (\Exception $e) {
+            return redirect()->back();            
+        }
+    }
+
+    /**
+     * Update the given TaskList.
+     *
+     * @param Illuminate\Http\Request $request
+     * @param App\Models\TaskList $list  (Value delivered by implicit route-model binding)
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TaskList $list)
+    {
+        $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        try {
+            $name = $request->name;
+
+            $updatedList = $list->updateTaskList($list, $name);
+
+            return $this->show($updatedList); // test that response is the list.show view (after show method is added)
 
         } catch (\Throwable $e) {
             return redirect()->back();
