1e08d3f6dc754e85414e1868e76074bf471540e1
Benjamen King <brk.3rdof3kings@gmail.com>
2017-10-31 21:11:08 -0600

    Create CategoryController and add 'store' method

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\TaskList;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Create a new Category instance and show it.
+     *
+     * @param Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'currentListID' => 'required|integer' // test validation failures make redirect
+        ]);
+
+        try {
+            $list = TaskList::find($request->currentListID);
+            $name = $request->name;
+
+            Category::newCategory($list, $name); // test a new category is created
+
+            $data = [
+                'user' => Auth::user(),
+                'list' => $list
+            ];
+
+            return view('list.show', $data); // test the view is returned and contains new cat. name
+
+        } catch (\Throwable $e) {
+            return redirect()->back();
+        } catch (\Exception $e) {
+            return redirect()->back();            
+        }
+    }
+
+    /**
+     * Update the given Category.
+     *
+     * @param Illuminate\Http\Request $request
+     * @param App\Models\Category $category
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function update(Request $request, Category $category)
+    // {
+    //     $request->validate([
+            
+    //     ]);
+
+    //     try {
+            
+
+    //     } catch (\Throwable $e) {
+    //         return redirect()->back();
+    //     } catch (\Exception $e) {
+    //         return redirect()->back();            
+    //     }
+    // }
+
+    /**
+     * Delete the given Category.
+     *
+     * @param App\Models\Category $category
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function destroy(Category $category)
+    // {
+    //     try {
+
+            
+
+    //     } catch (\Throwable $e) {
+    //         return redirect()->back();
+    //     } catch (\Exception $e) {
+    //         return redirect()->back();            
+    //     }
+    // }
+}
