ac81371a6ea50f1e81a8370eedf2084397cd06f2
Benjamen King <brk.3rdof3kings@gmail.com>
2017-11-01 13:32:50 -0600

    Create SubcategoryController

---
+++ app/Http/Controllers/SubcategoryController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\TaskList;
+use App\Models\Category;
+use App\Models\Subcategory;
+
+class SubcategoryController extends Controller
+{
+    /**
+     * Return the 'list.show' view.
+     *
+     * @param App\Models\TaskList $list
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private function showListView(TaskList $list)
+    {
+        $data = [
+                'user' => Auth::user(),
+                'list' => $list
+            ];
+
+            return view('list.show', $data);
+    }
+
+    /**
+     * Create a new Subcategory instance and show it.
+     *
+     * @param Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'categoryID' => 'required|string'
+        ]);
+
+        try {
+            $category = Category::find($request->categoryID);
+            $list = $category->taskList;
+            $name = $request->name;
+
+            Subcategory::newSubcategory($category, $name);
+
+            return $this->showListView($list);
+
+        } catch (\Throwable $e) {
+            return redirect()->back();
+        } catch (\Exception $e) {
+            return redirect()->back();            
+        }
+    }
+
+    /**
+     * Update the given Subcategory.
+     *
+     * @param Illuminate\Http\Request $request
+     * @param App\Models\Subcategory $subcategory
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subcategory $subcategory)
+    {
+        $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        try {
+            $list = $subcategory->category->taskList;
+            $name = $request->name;
+
+            $subcategory->updateSubcategory($name);
+
+            return $this->showListView($list);
+
+        } catch (\Throwable $e) {
+            return redirect()->back();
+        } catch (\Exception $e) {
+            return redirect()->back();            
+        }
+    }
+
+    /**
+     * Delete the given Subcategory.
+     *
+     * @param App\Models\Subcategory $subcategory
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subcategory $subcategory)
+    {
+        try {
+            $list = $subcategory->category->taskList;
+
+            $subcategory->deleteSubcategory();
+
+            return $this->showListView($list);
+
+        } catch (\Throwable $e) {
+            return redirect()->back();
+        } catch (\Exception $e) {
+            return redirect()->back();            
+        }
+    }
+}
