000353ad92608a6aec912df4dc917380beb6bae0
Benjamen King <brk.3rdof3kings@gmail.com>
2017-11-01 14:23:33 -0600

    Create TaskController and add 'store' method

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\TaskList;
+use App\Models\Category;
+use App\Models\Subcategory;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Return the 'list.show' view.
+     *
+     * @param App\Models\TaskList $list
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private function showListView(TaskList $list)
+    {
+        $data = [
+                'user' => Auth::user(),
+                'list' => $list
+            ];
+
+            return view('list.show', $data);
+    }
+
+    /**
+     * Create a new Task instance and show it.
+     *
+     * @param Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'deadline' => 'nullable|string',
+            'subcategoryID' => 'required|integer'
+        ]);
+
+        try {
+            $subcategory = Subcategory::find($request->subcategoryID);
+            $list = $subcategory->category->taskList;
+            $name = $request->name;
+            $deadline = $request->deadline;
+
+            Task::newTask($subcategory, $name, $deadline)
+
+            return $this->showListView($list);
+
+        } catch (\Throwable $e) {
+            return redirect()->back();
+        } catch (\Exception $e) {
+            return redirect()->back();            
+        }
+    }
+
+    /**
+     * Update the given Task's name or deadline.
+     *
+     * @param Illuminate\Http\Request $request
+     * @param App\Models\Task $task
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function updateDetails(Request $request, Task $task)
+    // {
+    //     $request->validate([
+            
+    //     ]);
+
+    //     try {
+            
+
+    //         return $this->showListView($list);
+
+    //     } catch (\Throwable $e) {
+    //         return redirect()->back();
+    //     } catch (\Exception $e) {
+    //         return redirect()->back();            
+    //     }
+    // }
+
+    /**
+     * Update the given Task's status.
+     *
+     * @param Illuminate\Http\Request $request
+     * @param App\Models\Task $task
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function updateStatus(Request $request, Task $task)
+    // {
+    //     $request->validate([
+            
+    //     ]);
+
+    //     try {
+            
+
+    //         return $this->showListView($list);
+
+    //     } catch (\Throwable $e) {
+    //         return redirect()->back();
+    //     } catch (\Exception $e) {
+    //         return redirect()->back();            
+    //     }
+    // }
+
+    /**
+     * Update the given Task's priority status.
+     *
+     * @param Illuminate\Http\Request $request
+     * @param App\Models\Task $task
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function updatePriority(Request $request, Task $task)
+    // {
+    //     $request->validate([
+            
+    //     ]);
+
+    //     try {
+            
+
+    //         return $this->showListView($list);
+
+    //     } catch (\Throwable $e) {
+    //         return redirect()->back();
+    //     } catch (\Exception $e) {
+    //         return redirect()->back();            
+    //     }
+    // }
+
+    /**
+     * Delete the given Task.
+     *
+     * @param App\Models\Task $task
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function destroy(Task $task)
+    // {
+    //     try {
+            
+
+    //         return $this->showListView($list);
+
+    //     } catch (\Throwable $e) {
+    //         return redirect()->back();
+    //     } catch (\Exception $e) {
+    //         return redirect()->back();            
+    //     }
+    // }
+}
