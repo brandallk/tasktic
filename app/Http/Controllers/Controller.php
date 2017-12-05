2a7a117dabb6b73d2477b7bedea9672f7ca09d27
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-05 09:16:25 -0700

    Add method 'tryToDo' to Controllers/Controller class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Throwable;
+use Exception;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function tryToDo(Closure $trialStatements)
+    {
+        try {
+            
+            $trialStatements();
+
+        } catch (Throwable $e) {
+
+            Log::error($e->__toString());
+            return view('errors.generalHttp');
+
+        } catch (Exception $e) {
+
+            Log::error($e->__toString());
+            return view('errors.generalHttp');
+        }
+    }
 }
